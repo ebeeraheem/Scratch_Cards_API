edb765ce0067bbfcd8b005d1c60a3534bf1f30d6
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-24 13:32:19 +0100

    feat: add database context
    Add the database context and created the ScratchCards table

---
+++ Scratch_Cards_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Scratch_Cards_API.Models;
+
+namespace Scratch_Cards_API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {        
+    }
+
+    public DbSet<ScratchCard> ScratchCards { get; set; }
+}
--- Scratch_Cards_API/Models/ScratchCard.cs
+++ Scratch_Cards_API/Models/ScratchCard.cs
@@ -3,7 +3,7 @@
 public class ScratchCard
 {
     public int Id { get; set; }
-    public string CardNumber { get; set; }
+    public string CardNumber { get; set; } = string.Empty;
     public bool IsPurchased { get; set; }
     public bool IsUsed { get; set; }
 }
