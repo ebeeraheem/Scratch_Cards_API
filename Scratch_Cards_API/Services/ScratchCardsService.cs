d19f682cbc75ea130e9781c255f3010f8d5f66bf
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-24 13:48:04 +0100

    feat: implement card services
    Add services to manage card generation,
    purchase, use, getting all cards
    and getting a card by it's id

--- Scratch_Cards_API/Services/ScratchCardsService.cs
+++ Scratch_Cards_API/Services/ScratchCardsService.cs
@@ -1,4 +1,6 @@
-﻿using Scratch_Cards_API.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Scratch_Cards_API.Data;
+using Scratch_Cards_API.Models;
 
 namespace Scratch_Cards_API.Services;
 
@@ -9,5 +11,64 @@ public class ScratchCardsService
     public ScratchCardsService(ApplicationDbContext context)
     {
         _context = context;
+    }
+
+    public async Task<ScratchCard> GenerateScratchCard()
+    {
+        var card = new ScratchCard()
+        {
+            CardNumber = Guid.NewGuid().ToString(),
+            IsPurchased = false,
+            IsUsed = false
+        };
+
+        await _context.ScratchCards.AddAsync(card);
+        await _context.SaveChangesAsync();
+
+        return card;
+    }
+
+    public async Task<List<ScratchCard>> GetAllCards()
+    {
+        return await _context.ScratchCards.ToListAsync();
+    }
+
+    public async Task<ScratchCard?> GetCardById(int id)
+    {
+        var card = await _context.ScratchCards.FindAsync(id);
+
+        return card;
+    }
+
+    public async Task<ScratchCard?> PurchaseCard(int id)
+    {
+        var card = await _context.ScratchCards.FindAsync(id);
+
+        if (card is null)
+        {
+            return null;
+        }
+
+        card.IsPurchased = true;
+
+        await _context.SaveChangesAsync();
+
+        return card;
+    }
+
+    public async Task<ScratchCard?> UseCard(int id)
+    {
+        var card = await _context.ScratchCards.FindAsync(id);
+
+        if (card is null)
+        {
+            return null;
+        }
+
+        card.IsUsed = true;
+
+        await _context.SaveChangesAsync();
+
+        return card;
     }
 }
