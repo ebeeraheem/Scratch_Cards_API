27d7a65942a46bae174563e1b7228153c77e4ce0
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-24 13:25:18 +0100

    feat: add scratch card model

---
+++ Scratch_Cards_API/Models/ScratchCard.cs
@@ -0,0 +1,9 @@
+﻿namespace Scratch_Cards_API.Models;
+
+public class ScratchCard
+{
+    public int Id { get; set; }
+    public string CardNumber { get; set; }
+    public bool IsPurchased { get; set; }
+    public bool IsUsed { get; set; }
+}
